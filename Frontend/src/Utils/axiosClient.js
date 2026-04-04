f66f20733c92e56bd286b72cd7846f8c27060d1e
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-04-05 00:15:22 +0530

    Update axiosClient.js

--- Frontend/src/Utils/axiosClient.js
+++ Frontend/src/Utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:5005',
+    baseURL: 'https://coding-platform-2-1.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
